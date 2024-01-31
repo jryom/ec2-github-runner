f8f18d7c9a0e98c2b14de621fc3bb8ac076a6eb3
Jesper Ryom <2834949+jryom@users.noreply.github.com>
2024-01-31 22:40:01 +0100

    Add keyname to config

--- src/aws.js
+++ src/aws.js
@@ -47,6 +47,7 @@ async function startEc2Instance(label, githubRegistrationToken) {
     SecurityGroupIds: [config.input.securityGroupId],
     IamInstanceProfile: { Name: config.input.iamRoleName },
     TagSpecifications: config.tagSpecifications,
+    KeyName: config.input.keyName,
   };
 
   try {
--- src/config.js
+++ src/config.js
@@ -15,6 +15,7 @@ class Config {
       iamRoleName: core.getInput('iam-role-name'),
       runnerHomeDir: core.getInput('runner-home-dir'),
       preRunnerScript: core.getInput('pre-runner-script'),
+      keyName: core.getInput('key-name'),
     };
 
     const tags = JSON.parse(core.getInput('aws-resource-tags'));
